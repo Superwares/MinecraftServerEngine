41835ae0721abc12be1fca3306d6048e1358a0f5
welcomehyunseo <welcomehyunseo@gmail.com>
2025-01-27 21:51:43 +0900

    save

--- TestMinecraftServerApplication/GameContextInventory.cs
+++ TestMinecraftServerApplication/GameContextInventory.cs
@@ -71,6 +71,32 @@ namespace TestMinecraftServerApplication
                     "Click to join the game",
                 ]));
 
+            SetSlot((9 * 4) + 0, new ItemStack(
+                ItemType.GrayStainedGlassPane, "Start Game!", 1, [
+                    "Click to start the game",
+                ]));
+            SetSlot((9 * 4) + 1, new ItemStack(
+                ItemType.RedStainedGlassPane, "First Round", 1, [
+                ]));
+            SetSlot((9 * 4) + 2, new ItemStack(
+                ItemType.RedStainedGlassPane, "Second Round", 1, [
+                ]));
+            SetSlot((9 * 4) + 3, new ItemStack(
+                ItemType.RedStainedGlassPane, "Third Round", 1, [
+                ]));
+            SetSlot((9 * 4) + 4, new ItemStack(
+                ItemType.RedStainedGlassPane, "Fourth Round", 1, [
+                ]));
+            SetSlot((9 * 4) + 5, new ItemStack(
+                ItemType.RedStainedGlassPane, "Fifth Round", 1, [
+                ]));
+            SetSlot((9 * 4) + 6, new ItemStack(
+                ItemType.RedStainedGlassPane, "Sixth Round", 1, [
+                ]));
+            SetSlot((9 * 4) + 7, new ItemStack(
+                ItemType.RedStainedGlassPane, "Final Round", 1, [
+                ]));
+
         }
 
 
